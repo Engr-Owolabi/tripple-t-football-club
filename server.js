2003b2c1d849bae73c66d513708f9a4577941f6f
Owolabi <saamtoop@gmail.com>
2026-08-27 20:19:43 +0100

    Fix PORT for Render deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 30001;
 
 // Middleware
 app.use(cors());
