b50f7959040f70b0c5fb6efcf6353de267c01686
Owolabi <saamtoop@gmail.com>
2026-08-29 00:02:06 +0100

    Fix website Not Found - Serve public folder until React build ready

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(MONGODB_URI)
   .then(() => console.log('✅ Connected to MongoDB Atlas!'))
